efd6f57850c101a3b23f642388ecf3d7325d7e9e
sciguy16@googlemail.com <6UBjA4tJpRrQnx8>
2021-11-21 14:36:59 -0600

    minor changes to test github login

--- data_env.py
+++ data_env.py
@@ -9,7 +9,6 @@ class Env:
     def __init__(self):
 
         percentagearr = []
-
         #Import data
         df = pd.read_csv("pbp-2020.csv")
         df1 = pd.read_csv("pbp-2019.csv")
@@ -21,14 +20,14 @@ class Env:
 
         # sorting all of the plays by which section of the field they are in
         for i in range(0,10):
-            arr = df[df.YardLine.between(0+i*10,10+i*10)]
+            arr = df[df.YardLine.between(0+i*10, 10+i*10)]
             percentagearr.append(arr)
 
         # further breaking those plays down into which down
         down_matrix = []
-        for i in range(0,10):
+        for i in range(0, 10):
             zone_matrix = []
-            for j in range(1,5):
+            for j in range(1, 5):
                 cur_data = percentagearr[i]
                 int_arr = cur_data[cur_data['Down'].isin([j])]
                 zone_matrix.append(int_arr)
